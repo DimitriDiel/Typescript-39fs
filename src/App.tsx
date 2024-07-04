02def8cc08d6906119671887e5f0541b2dc957eb
DimitriDiel <diel-d@web.de>
2024-07-04 20:18:32 +0400

    add Lesson 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 
 function App() {
-  return <div className="App">React typescript app</div>;
+  return (
+    <div className="App">
+      <Lesson_06 />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/lessons/Lesson_06/Lesson_06.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+function Lesson_06() {
+  let userFullname: string = "John Jonson";
+
+  return <div className="page_wrapper">Lesson 06</div>;
+}
+
+export default Lesson_06;
