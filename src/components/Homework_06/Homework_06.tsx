e51060bd589a4528d2634fdf18f613903f58d46b
DimitriDiel <diel-d@web.de>
2024-07-06 11:27:28 +0400

    add Lesson 07

--- src/components/Homework_06/Homework_06.tsx
+++ src/components/Homework_06/Homework_06.tsx
@@ -1,14 +1,9 @@
 import { v4 } from "uuid";
 import "./styles.css";
+import { Car } from "./types";
 
 function Homework_06() {
-  interface car {
-    brand: string;
-    price: number;
-    isDiesel: boolean;
-  }
-
-  const cars: car[] = [
+  const cars: Car[] = [
     { brand: "BMW", price: 20000, isDiesel: true },
     { brand: "Mercedes", price: 22000, isDiesel: false },
     { brand: "Porshe", price: 50000, isDiesel: true },
@@ -16,7 +11,7 @@ function Homework_06() {
     { brand: "Audi", price: 50000, isDiesel: true },
   ];
 
-  const addCards = cars.map((car) => {
+  const addCards = cars.map((car: Car) => {
     return (
       <div key={v4()} className="card">
         <h2>Brand name: {car.brand}</h2>
---
+++ src/lessons/Lesson_07/Lesson_07.tsx
@@ -0,0 +1,81 @@
+import "./styles.css";
+import { WEATHER_CODES } from "./types";
+
+function Lesson_07() {
+  const decode = (weatherCode: WEATHER_CODES): string => {
+    switch (weatherCode) {
+      case WEATHER_CODES.SQ: {
+        return "шквал";
+      }
+      case WEATHER_CODES.PO: {
+        return "пыльный вихрь";
+      }
+      case WEATHER_CODES.FC: {
+        return "торнадо";
+      }
+      case WEATHER_CODES.BR: {
+        return "дымка (видимость от 1 до 9 км)";
+      }
+      case WEATHER_CODES.HZ: {
+        return "мгла (видимость менее 10 км)";
+      }
+      case WEATHER_CODES.FU: {
+        return "дым (видимость менее 10 км)";
+      }
+      case WEATHER_CODES.DS: {
+        return "пыльная буря (видимость менее 10 км)";
+      }
+      case WEATHER_CODES.SS: {
+        return "песчаная буря (видимость менее 10 км) ";
+      }
+      default:
+        return "This code doesnt exist";
+    }
+  };
+
+  const showWeather = (
+    weatherCode: WEATHER_CODES,
+    decodeFunc: (weatherCode: WEATHER_CODES) => string
+  ) => {
+    console.log(decodeFunc(weatherCode));
+  };
+
+  showWeather(WEATHER_CODES.FC, decode);
+
+  type ArrayGenerator<ValueType> = ValueType[];
+
+  const someStringsArray: ArrayGenerator<string> = ["a", "b", "c"];
+  const numbers: ArrayGenerator<number> = [1, 2, 3];
+
+  interface ValueForMac {
+    model: string;
+    articul: number;
+  }
+
+  // Можно прокидывать не только 1 дженерик аргумент, а сколько угодно через запятую
+  interface ShopItems<ValueType> {
+    type: string;
+    value: ValueType;
+  }
+
+  const item1: ShopItems<number> = {
+    type: "TV",
+    value: 1235346245,
+  };
+
+  const item2: ShopItems<string> = {
+    type: "PC",
+    value: "Xiaomi Model 123",
+  };
+  const item3: ShopItems<ValueForMac> = {
+    type: "PC",
+    value: {
+      model: "Apple Pro Book",
+      articul: 1231241352345,
+    },
+  };
+
+  return <div>Lesson 07</div>;
+}
+
+export default Lesson_07;
